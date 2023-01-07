0554d2ecbf7570706a26bb8182bb333a88531fe0
Nikita Lylov <lylovn@gmail.com>
2023-01-07 07:16:20 +0300

    Three commit

--- src/Main.java
+++ src/Main.java
@@ -12,12 +12,13 @@ with fuel and different constructors
         // At (10;10) fuel=100
         Tank anywareTank = new Tank(10, 10);
         // At (20;30) fuel=200
-        Tank customTank = new Tank(20, 30, 200);
+        Tank customTank = new Tank();
         justTank.goForward(200);
         justTank.printPosition();
         anywareTank.goBackward(-200);
         anywareTank.printPosition();
         customTank.goForward(201);
+        customTank.printPosition();
         customTank.printPosition();
         customTank.printPosition();
         customTank.printPosition();
